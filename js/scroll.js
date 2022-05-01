e5ab595361e82f2fdb20e0738989b27cb0aa7616
Nico <72673830+NicolasHrt@users.noreply.github.com>
2022-05-01 20:04:17 +0200

    ajout de l'effet scroll sur la navbar

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+const navBarScroll = () => {
+    let derniere_position_de_scroll_connue = 0;
+    window.addEventListener('scroll', function(e) {
+        navbar = document.querySelector(".nav-bar")
+    
+        if(window.scrollY < derniere_position_de_scroll_connue ){
+            navbar.classList.remove("nav-bar-transform");
+        } else if(window.scrollY == 102){
+            console.log("hey");
+        } else if (window.scrollY >= 102) {
+            navbar.classList.add("nav-bar-transform");
+        } else if (window.scrollY <= 204) {
+            navbar.classList.remove("nav-bar-transform");
+        }
+        derniere_position_de_scroll_connue = window.scrollY;
+    
+    
+    })
+}
+
+navBarScroll();
+
