124f97049c8a3a1060ec504a0f534f7f8466c2fc
Nico <72673830+NicolasHrt@users.noreply.github.com>
2022-05-24 20:22:29 +0200

    add education section

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,9 @@
 const navSlide = () => {
-  console.log("yes");
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-list");
 
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-    console.log("bonjour");
   });
 };
 
